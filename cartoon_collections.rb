5d8835e70c6d827a5ed355c466b36ea640f8c438
Alexander Williams <williams_alex@protonmail.com>
2019-11-11 21:24:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(veggies)
-  veggies.map { |item| item.capitalize()
-  veggies.each{|veggie, index| veggies[index] = veggies[index].capitalize+"!"}
+  veggies.map { |item| item.capitalize+"!"}
   return veggies
 end
 
