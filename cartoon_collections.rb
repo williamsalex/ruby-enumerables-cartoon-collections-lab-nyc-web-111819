7e0a44f1ce10efc4b95d6e7ca284c33c91fd5473
Alexander Williams <williams_alex@protonmail.com>
2019-11-11 21:22:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(veggies)
-  veggies.each
+  veggies.each{|veggie, index| veggies[index] = 
 end
 
 def long_planeteer_calls# code an argument here
