0c775489a898be8167eb710604f3e553a8b9e699
Alexander Williams <williams_alex@protonmail.com>
2019-11-11 21:20:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index{|i, index| puts "#{index}. #{i}"}
+  array.each_with_index{|i, index| puts "#{index+1}. #{i}"}
 end
 
 def summon_captain_planet# code an argument here
